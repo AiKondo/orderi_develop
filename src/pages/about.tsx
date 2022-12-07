fb9d4d74410dc3f5244fdbc4d56bb6bd709b272b
AiKondo <ai_sugakiya03@icloud.com>
2022-12-07 18:14:09 +0900

    configured ESLint

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,7 @@
 const About = () => (
-    <div>
-      <h1>This page is About</h1>
-    </div>
-  );
-   
-  export default About;
-  
+  <div>
+    <h1>This page is About</h1>
+  </div>
+)
+
+export default About
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
-import Link from "next/link";
+import Link from 'next/link'
 export default function Home() {
   return (
     <div>
       <ul>
         <li>
-          <Link href="/about">
-            About
-          </Link>
+          <Link href="/about">About</Link>
         </li>
       </ul>
       <h1>Home</h1>
     </div>
-  );
+  )
 }
