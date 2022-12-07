299fe4e70ca96cdf3f9963654a51a0c425729353
AiKondo <ai_sugakiya03@icloud.com>
2022-12-07 15:54:10 +0900

    add _app.tsx

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  compiler:{
+    // styleComponentsの有効化
+    styledComponents: true,
+  },
 }
 
 module.exports = nextConfig
